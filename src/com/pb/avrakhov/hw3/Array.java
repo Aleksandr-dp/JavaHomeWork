9026602ea189fa7970748a785ed0e6f41ca97b26
Aleksandr Avrakhov <aleksandr.avrakhov@privatbank.ua>
2021-10-20 01:37:51 +0300

    Homework #3 - Cycles and Array

---
+++ src/com/pb/avrakhov/hw3/Array.java
@@ -0,0 +1,66 @@
+package com.pb.avrakhov.hw3;
+
+import java.util.Scanner;
+
+/*
+Создайте класс Array в пакете hw3. Программа должна позволить пользователю ввести одномерный массив целых чисел
+размерностью 10 элементов. Вывести на экран введенный массив. Подсчитать сумму всех элементов массива и вывести ее на
+экран. Подсчитать и вывести на экран количество положительных элементов. Произвести сортировку этого массива от меньшего
+к большему по алгоритму сортировки пузырьком. (https://en.wikipedia.org/wiki/Bubble_sort) Вывести на экран
+отсортированный массив.
+ */
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[10];          //Заданный массив
+        int counter = 0;                    //Счетчик
+        int sum = 0;                        //Сумма всех элементов массива
+        Scanner in = new Scanner(System.in);
+
+        //Запрашиваем у Пользователя массив
+        do {
+            System.out.print("Введите " + (counter + 1) + " число: ");
+            int userNumber = in.nextInt();
+            array[counter] = userNumber;
+            counter++;
+        } while (counter < 10);
+
+        //Выводим массив
+        System.out.print("Вы ввели следующие значения:\n");
+        for (int i : array) {
+            System.out.print(i + "  ");
+        }
+
+        //Выводим сумму всех элементов массива
+        System.out.print("\nСумма всех элементов массива равна: ");
+        for (int i : array) {
+            sum += i;
+        }
+        System.out.print(sum);
+
+        //Выводим количество положительных значений
+        System.out.print("\nКоличество положительных элементов: ");
+        counter = 0;
+        for (int i : array) {
+            if (i >= 0) {
+                counter++;
+            }
+        }
+        System.out.print(counter);
+
+        //Сортировка пузырьком (Bubble sort)
+        System.out.print("\nСортированнный массив:\n");
+        for (int i = (array.length - 1); i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i : array) {
+            System.out.print(i + "  ");
+        }
+    }
+}
---
+++ src/com/pb/avrakhov/hw3/Bingo.java
@@ -0,0 +1,74 @@
+package com.pb.avrakhov.hw3;
+
+import java.util.Scanner;
+
+/*
+Создайте класс Bingo в пакете hw3. Программа должна загадать целое число в диапазоне от 0 до 100 и предложить
+пользователю его отгадать. При вводе числа пользователем, программа проверяет на соответствие с загаданным число и если
+числа совпали вывести сообщение о том, что число отгадано. Если числа не совпали, тогда следует вывести надпись о том,
+что задуманное число является больше или меньше вводимого. Также программа ведет подсчет попыток, и выводит это
+количество после того как число угадали. Предусмотреть возможность досрочного завершения программы, в случае если
+пользователь не желает продолжать угадывать число.
+ */
+public class Bingo {
+    public static void main(String[] args) {
+        int hiddenNumber = (int) (Math.random() * 101); //Загаданное число
+        Scanner in = new Scanner(System.in);
+        int counter = 0;                                //Количество попыток (+1)
+        int[] array = new int[100];                     //Числа пользователя
+
+        System.out.print("Игра \u00ABBingo\u00BB. Отгадайте число от 0 до 100. Для выхода введите: 111.\n\n"); //Заголовок
+
+        while (true) {
+            System.out.print("Введите число от 0 до 100: ");
+            int userNumber = in.nextInt();                      //Число пользователя
+
+            //Проверка, если число выходит за заданный диапазон
+            if (userNumber < 0 || (userNumber > 100 && userNumber != 111)) {
+                System.out.print("Вы ввели не корректное число! (Диапозон от 0 до 100)\n");
+                continue;
+            }
+
+            //Выход из Игры
+            if (userNumber == 111) {
+                System.out.print("Вы не угадали загаданное число " + hiddenNumber + "\n" + "Количество попыток: " + counter);
+                break;
+            }
+
+            //Проверка, если Пользователь уже загадывал введенное число
+            if (counter == 0) {             //Если первая итерация, записываем значение
+                array[counter] = userNumber;
+            } else {
+                //Проверяем по массиву было ли это число ранее
+                for (int i = 0; i < counter; i++) {
+                    if (array[i] == userNumber) {
+                        System.out.print("Вы уже загадывали данное число:\n");
+
+                        //Вывод массива с попытками
+                        for (int j = 0; j < counter; j++) {
+                            System.out.print(array[j] + "  ");
+                        }
+
+                        System.out.print("\n");
+                        counter--;
+                        break;
+                    } else {
+                        array[counter] = userNumber;    //Записываем значение в массив
+                    }
+                }
+            }
+
+            //Проверяем число Пользователя
+            if (userNumber == hiddenNumber) {
+                System.out.print("Поздравляем! Вы угадали число: " + hiddenNumber + "\n" + "Количество попыток: " + (counter + 1));
+                break;
+            } else if (userNumber < hiddenNumber) {
+                System.out.print("Загаданное число больше " + userNumber + "\n");
+                counter++;
+            } else {
+                System.out.print("Загаданное число меньше " + userNumber + "\n");
+                counter++;
+            }
+        }
+    }
+}
