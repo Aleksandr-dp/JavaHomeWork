f05772cb9a669ae885302b4571f57e79c1190031
Aleksandr Avrakhov <aleksandr.avrakhov@privatbank.ua>
2021-11-22 17:30:20 +0200

    Homework 9 - Files - refac

--- src/com/pb/avrakhov/hw9/FileNumbers.java
+++ src/com/pb/avrakhov/hw9/FileNumbers.java
@@ -21,25 +21,28 @@ public class FileNumbers {
     static String data = "";
 
     public static void main(String[] args) throws IOException, NumberFormatException {
-        createNumbersFile();
-        createOddNumbersFile();
+        createNumbersFile();        //Создаем файл с числами
+        createOddNumbersFile();     //Меняем четные на 0
     }
 
     public static void createNumbersFile() {
         try (Writer writer = new FileWriter(PATH + "numbers.txt")){
+            //Создаем строки
             for (int j = 0; j < 10; j++){
+                //Заполняем числами
                 for (int i = 0; i < 10; i++) {
                     data += (int) (1 + Math.random() * 98) + " ";
                 }
                 data += "\n";
             }
 
-            writer.write(data);
+            writer.write(data);     //Заносим все в файл
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
     }
 
+    //Создаем новый файл с нечетными числами
     public static void createNumbersFile(String newData) {
         try (Writer writer = new FileWriter(PATH + "odd-numbers.txt")){
             writer.write(newData);
@@ -48,7 +51,7 @@ public class FileNumbers {
         }
     }
 
-    public static void createOddNumbersFile() throws IOException, NumberFormatException {
+    public static void createOddNumbersFile() throws NumberFormatException {
         Path path = Paths.get(PATH + "numbers.txt");
         data = "";
 
@@ -56,14 +59,15 @@ public class FileNumbers {
             List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
 
             for (String string: lines) {
-                String[] words = string.split(" ");
+                String[] words = string.split(" ");     //Получаем массив из чисел одной строки
 
-                for (int i = 0; i < words.length; i++) {
+                //Приводим к числу и проверяем на четность
+                for (String word: words) {
                     try {
-                        int integer = Integer.parseInt(words[i].trim());
+                        int integer = Integer.parseInt(word.trim());
 
                         if ((integer % 2) > 0) {
-                            data += words[i] + " ";
+                            data += word + " ";
                         } else {
                             data += "0 ";
                         }
@@ -75,7 +79,7 @@ public class FileNumbers {
                 data += "\n";
             }
 
-            createNumbersFile(data);
+            createNumbersFile(data);        //Создаем новый файл
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
